a4e53387521e925a207fe29c8dfc91643789822a
romanb123 <44100462+romanb123@users.noreply.github.com>
2023-02-18 14:22:48 +0200

    mode select section ui

--- index.php
+++ index.php
@@ -15,12 +15,20 @@
             <img src="./assets/images/user3.jpg" alt="avatar">
             <p class="user-name">User Name</p>
             <p class="user-email">test@test.com</p>
+            <div class="select-view">
+                <label for="mode-select"> <img src="./assets/images/icons/chat.png"> </label>
+                <label for="mode-select"> <img src="./assets/images/icons/contacts.png"> </label>
+                <label for="mode-select"> <img src="./assets/images/icons/settings.png"> </label>
+                <label for="mode-select"> <img src="./assets/images/icons/logout.png"> </label>
+            </div>
         </div>
         <div class="main">
             <div class="main_header">
                 <h1> Live Chat </h1>
             </div>
-            <div class="main_chats-list"></div>
+            <div class="main_chats-list">
+                <input type="checkbox" id="mode-select">
+            </div>
             <div class="main_single-chat"></div>
         </div>
     </div>
